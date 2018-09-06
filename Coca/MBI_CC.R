6a4cc1dddb90f12a0babe8745474497cfcbc02a1
arthurvaz <arthurvaz05@gmail.com>
2018-09-06 00:46:12 -0300

    ok

--- Coca/MBI_CC.R
+++ Coca/MBI_CC.R
@@ -13,9 +13,9 @@ MBI <- read_delim("~/Downloads/Mix_Bev_Industry.csv",
 #ADICIONAR A VARIAVEL MES
 MBI$Month <- with(MBI,lubridate::month(Date))
 #ADICIONAR A VARIAVEL PRECO
-MBI$Volume <- as.numeric(MBI$Volume)
-MBI$Value <- as.numeric(MBI$Value)
-MBI$PRICE <- with(MBI, Value/Volume)
+MBI$Volume <- round(as.numeric(gsub(",", ".", gsub("\\.", "", MBI$Volume))),2)
+MBI$Value <- round(as.numeric(gsub(",", ".", gsub("\\.", "", MBI$Value))),2)
+MBI$Price <- round(with(MBI, Value/Volume),2)
 #CATEGORIZAR AVG PACK SIZE POR 1Q/MEDIANA/3Q
 summary(MBI$`Avg. Pack.Size`)
 boxplot(MBI$`Avg. Pack.Size`)
@@ -29,5 +29,26 @@ MBI2 <-   cbind(with(MBI, model.matrix(~ AVG_PS + 0)), with(MBI, model.matrix(~ 
               with(MBI, model.matrix(~ `Pack Material` + 0)),MBI)
 MBI2 <- subset(MBI2, select = -c(`Avg. Pack.Size`,`Pack Material`,Flavor,`Caloric Content`,`Pack Material`,AVG_PS))
 #ANALISE DO MERCADO
-aggregate(MBI[,c("Date","Volume","Value","Price")])
-MBI$
+#VERIFICAR A CLASSE DAS VARIAVEIS
+sapply(MBI[,c("Date","Volume","Value","Price")],class)
+MBI$Date <- mdy(MBI$Date)
+MBI$Date <- as.Date(MBI$Date)
+
+mercado <- function(){
+  par(2,1)
+  cbind(MBI$Date,scale(MBI[,c("Value","Volume")]))%>%
+  aggregate(by = list(Group.date = MBI$Date), FUN=sum, na.rm=TRUE)%>%
+    ggplot() +
+    geom_line(aes(x=Group.date, y = Value, colour = "blue")) +
+    geom_line(aes(x=Group.date, y = Volume, colour = "green")) + 
+    scale_color_discrete(name = "Valores no tempo", labels = c("Valor", "Volume"))  
+    mutate(MBI, Price = Value/Volume)%>%  
+    ggplot() + 
+    geom_point(aes(x=Date, y = Price, colour = "red"))
+    
+    mutate(MBI[,c("Date","Value","Volume")], Price = Value/Volume)%>%
+    aggregate(by = list(Group.date = MBI$Date), FUN=sum, na.rm=TRUE)%>%
+    print()
+}
+mercado()
+
